430ba625dbe17b18948bc8f32f6f4c2a11b04bdc
dev-talukdar <khejurbd@gmail.com>
2023-05-18 15:15:35 +0600

    all cars page added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer/Footer";
+import NavigationBar from "../Pages/Shared/NavigationBar/NavigationBar";
 
  
 
 const Main = () => {
     return (
         <div>
+            <NavigationBar></NavigationBar>
            <Outlet></Outlet> 
            <Footer></Footer>
         </div>
