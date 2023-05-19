f3171d10f97ceb30b22bea126312e2ee9b638fbf
dev-talukdar <khejurbd@gmail.com>
2023-05-19 16:04:18 +0600

    gallery now added

--- src/Pages/Home/Gallary/Gallary.jsx
+++ src/Pages/Home/Gallary/Gallary.jsx
@@ -1,30 +1,56 @@
-import { useEffect, useState } from "react";
-import GallaryCard from "./GallaryCard";
-
- 
+import { useState, useEffect } from 'react';
 
 const Gallary = () => {
-    const [gallery, setGallery] = useState([]);
-    
+  const [gallery, setGallery] = useState([]);
+  const [showAllCards, setShowAllCards] = useState(false);
 
-    useEffect( () => {
-        fetch('cars.json')
-        .then(res => res.json())
-        .then(data => setGallery(data))
-    }, [])
-    return (
-        <div>
-            <h2 className="text-center text-4xl font-bold mt-16 mb-12">Trending Cars</h2>
-            <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                {
-                gallery.map(cars => <GallaryCard
-                key={gallery._id}
-                cars={cars}
-                ></GallaryCard> )
-                }
-            </div>
+  useEffect(() => {
+    fetch('gallery.json')
+      .then(res => res.json())
+      .then(data => setGallery(data))
+  }, []);
+
+  const displayedCards = showAllCards ? gallery : gallery.slice(0, 6);
+
+  const handleViewMore = () => {
+    setShowAllCards(true);
+  };
+
+  return (
+    <div>
+      <h2 className="text-center text-4xl font-bold mt-16 mb-12">Trending Cars</h2>
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {displayedCards.map(cars => (
+          <GallaryCard key={cars._id} cars={cars} />
+        ))}
+      </div>
+      {!showAllCards && (
+        <div className="flex justify-center btn-block mt-12 mb-12">
+          <button className="btn bg-orange-600 border-none" onClick={handleViewMore}>
+            View More
+          </button>
         </div>
-    );
+      )}
+    </div>
+  );
 };
+
+const GallaryCard = ({ cars }) => {
+  const { pictureUrl, name } = cars;
+
+  return (
+    <div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <figure className="px-10 pt-10">
+          <img src={pictureUrl} alt="Car" className="rounded-xl" />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{name}</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 
 export default Gallary;
--- src/Pages/Home/Gallary/GallaryCard.jsx
+++ src/Pages/Home/Gallary/GallaryCard.jsx
@@ -1,21 +1,21 @@
+  
+const GallaryCard = ({ cars }) => {
+    const { pictureUrl, name } = cars;
+   
 
-
-const GallaryCard = ({cars}) => {
-    const {picture, name, price, rating} = cars;
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
-                <img src={picture} alt="Shoes" className="rounded-xl" />
-            </figure>
-            <div className="card-body items-center text-center">
-                <h2 className="card-title">{name}</h2>
-                <p>Price $ {price}</p>
-                <p>Rating {rating}</p>
-                <div className="card-actions">
-                    <button className="btn bg-orange-600 border-none">View Details</button>
+         <div className="card-container">
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure className="px-10 pt-10">
+                    <img src={pictureUrl} alt="Shoes" className="rounded-xl" />
+                </figure>
+                <div className="card-body items-center text-center">
+                    <h2 className="card-title">{name}</h2> 
                 </div>
+            </div> 
+            
+            
             </div>
-        </div>
     );
 };
 
